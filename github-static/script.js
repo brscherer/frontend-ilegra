c962d40d784e4e9ef0be759a89e986e0bd475142
Bruno Scherer <bruno.raphael96@gmail.com>
2018-09-21 09:47:41 -0300

    done: navbar github

--- github-static/script.js
+++ github-static/script.js
@@ -1,9 +1,14 @@
-const el = document.querySelector("[data-toggle]");
+const elements = document.querySelectorAll("[data-toggle]");
 
-el.addEventListener('click', e => {
-	e.preventDefault();
-	const attr = el.getAttribute("data-toggle");
-	const target = document.querySelector(`#${attr}`);
+elements.forEach( el => {
+	el.addEventListener('click', e => {
+		e.preventDefault();
+		const attr = el.getAttribute("data-toggle");
+		const target = document.querySelector(`#${attr}`);
+		
+		el.classList.toggle('actived');
+		target.classList.toggle('actived');
 
-	target.classList.toggle('actived');
-});
+		
+	});
+})
