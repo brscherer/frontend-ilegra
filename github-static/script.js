b008f51c1b405d127ad902860dcdb7deb981d5fa
Thiago Galvani <thiagopaiva99@gmail.com>
2018-09-21 18:13:03 -0300

    Comentários a respeito da avaliação do código

--- github-static/script.js
+++ github-static/script.js
@@ -3,11 +3,11 @@ const elements = document.querySelectorAll("[data-toggle]");
 elements.forEach( el => {
 	el.addEventListener('click', e => {
 		e.preventDefault();
-		const attr = el.getAttribute("data-toggle");
-		const target = document.querySelector(`#${attr}`);
+		const attr = el.getAttribute("data-toggle"); // Manter um padrão se vai usar "" ou ''
+		const target = document.querySelector(`#${attr}`); // Aqui ta fixo o id, sendo assim, da tranquilo pra usar um document.getElementById(attr)
 		
 		el.classList.toggle('actived');
 		target.classList.toggle('actived');
-
+		// Da pra eliminar essas linhas assim que nao são usadas
 	});
-})
+}) // Manter um padrão se vai usar ";" ou não
