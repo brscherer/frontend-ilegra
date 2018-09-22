d78470ef17671e57161f7e59d0e5c08d918bb2e2
Bruno Scherer <bruno.raphael96@gmail.com>
2018-09-22 14:48:38 -0300

    fix: remove !important from css & remove unused lines from js

--- github-static/script.js
+++ github-static/script.js
@@ -3,11 +3,7 @@ const elements = document.querySelectorAll("[data-toggle]");
 elements.forEach( el => {
 	el.addEventListener('click', e => {
 		e.preventDefault();
-		const attr = el.getAttribute("data-toggle");
-		const target = document.querySelector(`#${attr}`);
-		
 		el.classList.toggle('actived');
-		target.classList.toggle('actived');
-
+		document.getElementById(el.getAttribute('data-toggle')).classList.toggle('actived');
 	});
-})
+});
